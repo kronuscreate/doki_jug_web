77ef93af40262bcb9d07416da550c39b8e55636a
Kusumi <ryu-0916-luv_kths@docomo.ne.jp>
2025-08-08 15:49:08 +0900

    [dev] 構造の修正、パスの修正

---
+++ src/body.ts
@@ -0,0 +1,37 @@
+class Body extends Phaser.GameObjects.Container {
+
+    private top: Phaser.GameObjects.Image | null = null;
+    private topPanel: Phaser.GameObjects.Image | null = null;
+    private middlePanel: Phaser.GameObjects.Image | null = null;
+    private middleLedOff: Phaser.GameObjects.Image | null = null;
+    private mainPanel: Phaser.GameObjects.Image | null = null;
+    private controlPanel: Phaser.GameObjects.Image | null = null;
+    private bottom: Phaser.GameObjects.Image | null = null;
+    private bottomPanel: Phaser.GameObjects.Image | null = null;
+
+    constructor(scene: Phaser.Scene) {
+        super(scene);
+        this.create();
+    }
+
+    create(): void {
+        this.top = new Phaser.GameObjects.Image(this.scene, 24, 1, 'top').setOrigin(0, 0);
+        this.topPanel = new Phaser.GameObjects.Image(this.scene, 92, 271, 'top_panel').setOrigin(0, 0);
+        this.middlePanel = new Phaser.GameObjects.Image(this.scene, 23, 507, 'middle_panel').setOrigin(0, 0);
+        this.middleLedOff = new Phaser.GameObjects.Image(this.scene, 23, 507, 'middle_led_off').setOrigin(0, 0);
+        this.mainPanel = new Phaser.GameObjects.Image(this.scene, 13, 642, 'main_panel').setOrigin(0, 0);
+        this.controlPanel = new Phaser.GameObjects.Image(this.scene, 10, 1349, 'control_panel').setOrigin(0, 0);
+        this.bottom = new Phaser.GameObjects.Image(this.scene, 0, 1607, 'bottom').setOrigin(0, 0);
+        this.bottomPanel = new Phaser.GameObjects.Image(this.scene, 61, 1650, 'bottom_panel').setOrigin(0, 0);
+
+        this.add(this.top);
+        this.add(this.topPanel);
+        this.add(this.middlePanel);
+        this.add(this.middleLedOff);
+        this.add(this.mainPanel);
+        this.add(this.controlPanel);
+        this.add(this.bottom);
+        this.add(this.bottomPanel);
+    }
+}
+export default Body;
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,40 @@
 import Phaser from 'phaser';
+import Body from './body';
 
-class DemoScene extends Phaser.Scene {
+class SlotScene extends Phaser.Scene {
   constructor() {
-    super('demo');
+    super('slot-scene');
   }
 
   preload(): void {
-    this.load.image('top', 'public/assets/images/top.png');
-    this.load.image('top_panel', 'public/assets/images/top_panel.png');
-    this.load.image('middle_panel', 'public/assets/images/middle_panel.png');
-    this.load.image('middle_led_off', 'public/assets/images/middle_led_off.png');
-    this.load.image('middle_led_on', 'public/assets/images/middle_led_on.png');
-    this.load.image('main_panel', 'public/assets/images/main_panel.png');
-    this.load.image('control_panel', 'public/assets/images/control_panel.png');
-    this.load.image('bottom', 'public/assets/images/bottom.png');
-    this.load.image('bottom_panel', 'public/assets/images/bottom_panel.png');
-
+    this.load.image('top', '/public/assets/images/top.png');
+    this.load.image('top_panel', '/public/assets/images/top_panel.png');
+    this.load.image('middle_panel', '/public/assets/images/middle_panel.png');
+    this.load.image('middle_led_off', '/public/assets/images/middle_led_off.png');
+    this.load.image('middle_led_on', '/public/assets/images/middle_led_on.png');
+    this.load.image('main_panel', '/public/assets/images/main_panel.png');
+    this.load.image('control_panel', '/public/assets/images/control_panel.png');
+    this.load.image('bottom', '/public/assets/images/bottom.png');
+    this.load.image('bottom_panel', '/public/assets/images/bottom_panel.png');
   }
 
   create(): void {
-    this.add.image(24, 1, 'top').setOrigin(0, 0);
-    this.add.image(92, 271, 'top_panel').setOrigin(0, 0);
-    this.add.image(23, 507, 'middle_panel').setOrigin(0, 0);
-    this.add.image(23, 507, 'middle_led_off').setOrigin(0, 0);
-    this.add.image(13, 642, 'main_panel').setOrigin(0, 0);
-    this.add.image(10, 1349, 'control_panel').setOrigin(0, 0);
-    this.add.image(0, 1607, 'bottom').setOrigin(0, 0);
-    this.add.image(61, 1650, 'bottom_panel').setOrigin(0, 0);
+    const body = new Body(this);
+    this.add.existing(body);
   }
 }
 
 const game = new Phaser.Game({
   type: Phaser.AUTO,
   parent: 'app',
-  backgroundColor: '#242424',
+  backgroundColor: '#000000',
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 1578,
     height: 2672
   },
-  scene: [DemoScene]
+  scene: [SlotScene]
 });
 
 export default game;
